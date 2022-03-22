b9f2ca8947093370ebdba480809fcc554e98e553
KievanKartono <100666767+KievanKartono@users.noreply.github.com>
2022-03-22 22:33:52 +0800

    Create main.c

---
+++ hw2/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+typedef struct lotto_record {
+    int emp_id;
+    int lotto_no;
+    int lotto_receipt;
+    char lotto_date[32];
+    char lotto_time[32];
+} lotto_record_t;
+
+typedef struct emp_record {
+  int emp_id;
+  char emp_name[32];
+  int emp_salary;
+} emp_record_t;
+
+int main() {
+    FILE* fp;
+    char name[16];
+    int T,a,n,operator[]={-1},b;
+    lotto_record_t record[1];
+    T=7;
+    a=5;
+    srand(T);
+    time_t curtime;
+    time(&curtime);
+    printf("Operator Id (0-5)=");
+    scanf("%d", &operator[0]);
+    
+    if (operator[0] == 0) {
+    emp_record_t employees[100];
+    if ((fp = fopen("operator_id.bin", "r")) != NULL) fread(employees, sizeof(emp_record_t), sizeof(employees), fp);
+    while (id < 1 || id > 99) {
+      printf("ID（1－99）：");
+      scanf("%d", &id);
+    }
+    employees[id].emp_id = id;
+    printf("Name：");
+    scanf("%s", employees[id].emp_name);
+    printf("Salary：");
+    scanf("%d", &employees[id].emp_salary);
+    fp = fopen("operator_id.bin", "wb+");
+    fwrite(employees, sizeof(emp_record_t), sizeof(employees), fp);
+    fclose(fp);
+    printf("Complete\n");
+    } else {
+    printf("Number (1-5)=");
+    scanf("%d", &n);
+    fclose(fp);
+    for (int i=1,j=0; j==0; i++) {
+        sprintf(name, "lotto[%04d].txt",i);
+        if (access(name,F_OK)!=0) {
+            fp = fopen(name, "w+");
+            fprintf(fp, "======= Lotto 659 ========\n");
+            fprintf(fp, "=======+ No.%05d +======\n", i);
+            b = i;
+            j=1;
+        };
+    }
+    fprintf(fp, "%s", ctime(&curtime));
+    for(int j=1; j<a+1; j++) {
+        fprintf(fp, "[%d]: ", j);
+        if(j<n+1) {
+        for(int k=0; k<T; k++) {
+            if(k < 5) {
+                fprintf(fp, "%02d ", rand() % 69);
+            } else {
+                fprintf(fp, "%02d ", rand() % 10);
+            }
+        }
+    } else {
+    for(int k=0; k<T; k++) {
+        fprintf(fp, "-- ");
+    }
+}
+    fprintf(fp, "\n");
+    }
+    fprintf(fp, "=======* Op.%05d *=========\n", operator[0]);
+    fprintf(fp, "======= csie@CGU =========\n");
+    fclose(fp);
+
+    record[0].emp_id = operator[0];
+    record[0].lotto_no = b;
+    record[0].lotto_receipt = n*50*1.1;
+    strftime(record[0].lotto_date, 100, "%Y%m%d", localtime(&curtime));
+    strftime(record[0].lotto_time, 100, "%H:%M:%S", localtime(&curtime));
+    fp=fopen("records.bin", "ab+");
+    fwrite(record, sizeof(int), sizeof(record), fp);
+    fclose(fp);
+
+    printf("Your output will be at %s.txt\n", name);
+    return 0;
+}
