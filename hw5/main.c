2de07113cf8a7ec9ddc009c2fa1abcc5258f8afe
KievanKartono <100666767+KievanKartono@users.noreply.github.com>
2022-04-17 17:01:47 +0800

    Update main.c

--- hw5/main.c
+++ hw5/main.c
@@ -58,13 +58,13 @@ void my_free(int* p) {
 }
     
 int main() {
-	int *np1 = my_calloc(1, 1);
-	int *np2 = my_calloc(1, 2);
-	int *np3 = my_calloc(1, 3);
-	int *np4 = my_calloc(1, 4);
-	my_free(np1);
-	my_free(np4);
-	int *np5 = my_calloc(1, 5);
+  int *np1 = my_calloc(1, 1);
+  int *np2 = my_calloc(1, 2);
+  int *np3 = my_calloc(1, 3);
+  int *np4 = my_calloc(1, 4);
+  my_free(np1);
+  my_free(np4);
+  int *np5 = my_calloc(1, 5);
   int* np6 = my_calloc(1, 1);
   return 0;
 }
